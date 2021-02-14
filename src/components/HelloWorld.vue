130cd0380d83704aca07bbc6af6def96c182687e
Shohei Ebisuda <>
2021-02-14 13:46:56 +0900

    canvas内に画像を移動させる。一回すべて削除しないとダメっぽい

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="hello">
+		<div class="upload">
+			<input type="text" name="text" id="text" v-model="text">
+			<input type="file" name="file" id="file" @change="uploadFile">
+		</div>
+		<button @click="drawImage(0,0)">画像を送信する</button>
+			<input type="range" name="text" v-model="uploadedImage.x" @input='dx'>
+			<input type="range" name="text" v-model="uploadedImage.y" @input='dy'>
+		<button @click="drawText">テキストを送信する</button>
 		<canvas id="test" width=900 height=1600></canvas>
 		<!--		<img src ="../assets/logo.png">-->
     <h1>{{ msg }}</h1>
@@ -12,7 +20,13 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js App',
+      text: '',
+			uploadedImage : {
+				path: false,
+				x: 0,
+				y: 0,
+			},
     }
   },
 	//mountedはすべて読み込まれてから発火。createdは読み込まれる前に発火
@@ -21,9 +35,17 @@ export default {
 		//描写オブジェクトの呼び出し
 		const ctx = canvas.getContext('2d');
 
-		this.insertImage(ctx, require("../assets/01.png"), 10, 40);
 
+		show().then(console.log('hopge'));
 
+		async function getData(){
+			console.log('ho')
+			return 'success';
+		}
+
+		async function show(){
+			const data = await getData();
+		}
 
 	},
 	methods :{
@@ -47,6 +69,43 @@ export default {
 			ctx.font = "8px YuGothic";
 			ctx.fillText("(税込)", y + 86, 31);
 		},
+		uploadFile: function(event){
+			const files = event.target.files
+			const reader = new FileReader();
+			//なぜか必要理由を調べるべき
+			reader.readAsDataURL(files[0]);
+
+			reader.onload = e => {
+				this.uploadedImage.path = e.target.result;
+			};
+		},
+		drawImage: function(x, y){
+			const canvas = document.getElementById('test');
+			//描写オブジェクトの呼び出し
+			const ctx = canvas.getContext('2d');
+				this.insertImage(ctx, this.uploadedImage.path, x, y);
+		},
+		drawText: function(){
+			const canvas = document.getElementById('test');
+			//描写オブジェクトの呼び出し
+			const ctx = canvas.getContext('2d');
+			ctx.font = "bold 64px YuGothic";
+			ctx.fillText(this.text, 100,100);
+		},
+		dx: function(){
+			const canvas = document.getElementById('test');
+			//描写オブジェクトの呼び出し
+			const ctx = canvas.getContext('2d');
+			ctx.clearRect(0, 0, canvas.width, canvas.height);
+			this.drawImage(this.uploadedImage.x, this.uploadedImage.y)
+		},
+		dy: function(){
+			const canvas = document.getElementById('test');
+			//描写オブジェクトの呼び出し
+			const ctx = canvas.getContext('2d');
+			ctx.clearRect(0, 0, canvas.width, canvas.height);
+			this.drawImage(this.uploadedImage.x, this.uploadedImage.y)
+		},
 	},
 }
 
@@ -54,6 +113,9 @@ export default {
 <style>
 #test{
 	border: 1px solid black;
+    background-image: url("~@/../static/back.jpg");
+}
+.hello{
 }
 </style>
 
