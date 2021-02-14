1d64d4b25eb945460565d8bfb9060802c645b28a
Shohei Ebisuda <>
2021-02-14 12:07:09 +0900

    refactoring unused code

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -21,27 +21,9 @@ export default {
 		//描写オブジェクトの呼び出し
 		const ctx = canvas.getContext('2d');
 
-		this.insertImage(ctx, 'https://mdn.mozillademos.org/files/5395/backdrop.png', 10, 40);
-			//Vue.jsのコンポーネントは相対パスの場合、Base64エンコードされずにそのまま出力される。
-			//そのためrequireで囲んで上げる必要がある
-			//http://tk2000ex.blogspot.com/2017/11/vue.html
-		this.insertImage(ctx, require("../assets/logo.png"), 10, 40);
-
-		this.insertSmallImage(ctx, require('@/assets/01.png'), 30, 40, 450, 500)
-
-		//canvasに文字を記載する
-		ctx.font = "32px Comic Sans MS";
-		const y = 350;
-		ctx.strokeStyle = '#f00';
-		ctx.strokeText("2,200", y, 50);
+		this.insertImage(ctx, require("../assets/01.png"), 10, 40);
 
 
-
-		ctx.font = "bold 16px YuGothic";
-		ctx.fillText("円", y + 88, 50);
-
-		ctx.font = "8px YuGothic";
-		ctx.fillText("(税込)", y + 86, 31);
 
 	},
 	methods :{
@@ -52,12 +34,18 @@ export default {
 				ctx.drawImage(img, dx, dy);
 			};
 		},
-		insertSmallImage: function(ctx, img_path, dx, dy, w, h){
-			var img = new Image();
-			img.src = img_path;
-			img.onload = () => {
-				ctx.drawImage(img, dx, dy, w, h);
-			};
+		drawMoney: function(ctx, img_path, dx, dy){
+			//canvasに文字を記載する
+			ctx.font = "32px Comic Sans MS";
+			const y = 350;
+			ctx.strokeStyle = '#f00';
+			ctx.strokeText("2,200", y, 50);
+
+			ctx.font = "bold 16px YuGothic";
+			ctx.fillText("円", y + 88, 50);
+
+			ctx.font = "8px YuGothic";
+			ctx.fillText("(税込)", y + 86, 31);
 		},
 	},
 }
